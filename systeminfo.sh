e64dee0f83d9cc2d5d15446bd6b76a44b38eed92
FlumeZ <35439776+xFlum3@users.noreply.github.com>
2022-03-01 19:21:04 +0200

    Update systeminfo.sh
    - Added Kali linux Support

--- systeminfo.sh
+++ systeminfo.sh
@@ -44,13 +44,37 @@ elif test $os_type = "Raspbian"; then
  ':,  ''  ,:'
    ' ~::~ '
 ${NC}"
+elif test $os_type = "Kali"; then
+	echo -e "${BLUE}
+	 ..............
+            ..,;:ccc,.
+          ......''';lxO.
+.....''''..........,:ld;
+           .';;;:::;,,.x,
+      ..'''.            0Xxoc:,.  ...
+  ....                ,ONkc;,;cokOdc',.
+ .                   OMo           ':${c2}dd${c1}o.
+                    dMc               :OO;
+                    0M.                 .:o.
+                    ;Wd
+                     ;XO,
+                       ,d0Odlc;,..
+                           ..',;:cdOOd::,.
+                                    .:d;.':;.
+                                       'd,  .'
+                                         ;l   ..
+                                          .o
+                                            c
+                                            .'
+                                             .
+${NC}"
 else
   echo "Cannot Found OS Type."
 fi
 
 #User & hostname display
 echo -e "${RED} $USER@$HOSTNAME"
-echo -e "${NC} -------------------------"
+echo -e "${BLUE}-------------------------${NC}"
 
 #OS
 os=$(hostnamectl | grep "Operating" | cut -d":" -f2)
@@ -60,6 +84,8 @@ echo -e "${RED} OS ${NC}: $os $architecture"
 #Desktop Environment
 de=$(echo $XDG_CURRENT_DESKTOP | cut -d":" -f2)
 if test $os_type = "Ubuntu"; then
+	echo -e "${RED} DE ${NC}: $de"
+elif test $os_type = "Kali"; then
 	echo -e "${RED} DE ${NC}: $de"
 fi
 
@@ -98,15 +124,32 @@ else
 fi
 
 #Shell
-shell=$(bash --version | grep "version" | cut -d" " -f4 | head -n1 | cut -d"(" -f1)
-type=$(echo "$SHELL" | cut -d"/" -f3)
-echo -e "${RED} Shell ${NC}: $type $shell"
+if test $os_type = "Ubuntu"; then
+	shell=$(bash --version | grep "version" | cut -d" " -f4 | head -n1 | cut -d"(" -f1)
+	type=$(echo "$SHELL" | cut -d"/" -f3)
+	echo -e "${RED} Shell ${NC}: $type $shell"
+elif test $os_type = "Debian"; then
+	shell=$(bash --version | grep "version" | cut -d" " -f4 | head -n1 | cut -d"(" -f1)
+	type=$(echo "$SHELL" | cut -d"/" -f3)
+	echo -e "${RED} Shell ${NC}: $type $shell"
+elif test $os_type = "Raspbian"; then
+	shell=$(bash --version | grep "version" | cut -d" " -f4 | head -n1 | cut -d"(" -f1)
+	type=$(echo "$SHELL" | cut -d"/" -f3)
+	echo -e "${RED} Shell ${NC}: $type $shell"
+elif test $os_type = "Kali"; then
+	shell=$(bash --version | grep "version" | cut -d" " -f4 | head -n1 | cut -d"(" -f1)
+	type=$(echo "$SHELL" | cut -d"/" -f4)
+	echo -e "${RED} Shell ${NC}: $type $shell"
+fi
 
 #Resolution (Cannot Support RaspberrPI)
 if test $os_type = "Ubuntu"; then
 	res=$(xdpyinfo | grep dimensions | cut -d":" -f2 | cut -d"(" -f1 | cut -d" " -f5-6)
 	echo -e "${RED} Resolution ${NC}: $res"
 elif test $os_type = "Debian"; then
+	res=$(xdpyinfo | grep dimensions | cut -d":" -f2 | cut -d"(" -f1 | cut -d" " -f5-6)
+	echo -e "${RED} Resolution ${NC}: $res"
+elif test $os_type = "Kali"; then
 	res=$(xdpyinfo | grep dimensions | cut -d":" -f2 | cut -d"(" -f1 | cut -d" " -f5-6)
 	echo -e "${RED} Resolution ${NC}: $res"
 fi
@@ -120,6 +163,9 @@ if test $os_type = "Ubuntu"; then
 	gpu=$(lspci | grep VGA | cut -d" " -f5-8)
 	echo -e "${RED} GPU ${NC}: $gpu"
 elif test $os_type = "Debian"; then
+	gpu=$(lspci | grep VGA | cut -d" " -f5-8)
+	echo -e "${RED} GPU ${NC}: $gpu"
+elif test $os_type = "Kali"; then
 	gpu=$(lspci | grep VGA | cut -d" " -f5-8)
 	echo -e "${RED} GPU ${NC}: $gpu"
 fi
